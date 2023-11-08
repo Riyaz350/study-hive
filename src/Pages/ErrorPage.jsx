db6bfe83b22877624eadb7ffb49921e5f9ba9b8c
riyaz <riyaz3582@gmail.com>
2023-11-08 16:58:22 +0600

    added error page

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import Navbar from '../Shared/Navbar';
+import Footer from '../Shared/Footer';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className='w-full max-w-7xl mx-auto'>
+                <img className='w-1/2 mx-auto' src="https://i.ibb.co/tDzC470/Animation-1699440465960-2.gif" alt="" />
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,4 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import {createBrowserRouter,} from "react-router-dom";
 import Root from "../Root/Root";
 import LogIn from "../Pages/LogIn";
 import Register from "../Pages/Register";
@@ -12,12 +10,14 @@ import AssignmentDetails from "../Pages/Assignments/AssignmentDetails";
 import SubmittedAssignments from "../Pages/SubmittedAssignments/SubmittedAssignments";
 import PrivateRoutes from "./PrivateRoutes";
 import Home from "../Pages/Home/Home";
+import ErrorPage from "../Pages/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root>,
+      errorElement:<ErrorPage></ErrorPage>,
       children: [
         {
             path:'/',
